184d7974093ed9dc24a2935fde76720544d3770c
Lola Quiroga <lolaquiroga25@gmail.com>
2025-10-10 11:27:25 -0700

    added env variables

--- api_request.py
+++ api_request.py
@@ -1,6 +1,11 @@
 import http.client
 import csv
 import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
 
 # args: route (str), percentile (int), start date ("MM-DD-YYYY"), end date ("MM-DD-YYYY"), days of week (comma delimited, no spaces)
 def call(route=11, percentile=60, start_date ="03-13-2025", end_date="06-18-2025", days_of_week="1,2,3,4,5", dir=1):
@@ -8,7 +13,7 @@ def call(route=11, percentile=60, start_date ="03-13-2025", end_date="06-18-2025
 
     headers = {
         'Accept': "application/json, text/csv; charset=utf-8, */*",
-        'Authorization': "52d1916b5d1221ed54267b01e6a47de5"
+        'Authorization': api_key
     }
 
     conn.request("GET", f"/run-times/santa-cruz/path-stats?routes={route}&additionalGroupBy=SCHEDULED_TRIP_START_TIME,SCHEDULED_RUN_TIME&aggregationTypes=PERCENTILE&aggregationFields=RUN_TIME&listAggregations=SCHEDULED_RUN_TIME&percentiles={percentile}&groupByTimepoint=true&startDate={start_date}&endDate={end_date}&daysOfWeek={days_of_week}&directionId={dir}", headers=headers)
