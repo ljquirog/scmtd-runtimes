30e4c98e895ecc22be7e001fd002d9ca2d25bb72
Lola Quiroga <lolaquiroga25@gmail.com>
2025-10-22 11:55:40 -0700

    comment in main.py

--- main.py
+++ main.py
@@ -2,4 +2,16 @@ import runtime_suggestions_by_percentile
 import percentiles_test
 
 if __name__ == "__main__":
-    print("hai")
+    '''
+    For UCSC routes, the seasonal variation is significant enough that we’ll likely need four schedules per year, and eventually move to distinct M/W/F and T/Th schedules. 
+    
+    For non-UCSC routes, if the difference in run times between winter and fall/winter/spring is minimal, then a two-schedule approach may be sufficient.
+
+    I’d be interested in seeing a comparison of winter runtimes (for non-UCSC routes) based on:
+        Last winter’s data, and
+        Combined fall, winter, and spring bids.
+    
+'''
+    # run last winter 
+    # run combined runtimes
+    # have walter manually calculate the diff?
